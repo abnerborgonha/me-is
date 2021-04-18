b7229efa73c7eae58448f210ee53bbd3b7b6fc4b
Abnêr Borgonha <abner@triider.com>
2021-04-18 20:02:27 -0300

    feat> create doctor js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
+
 import { Container, Name } from '../styles/pages/Home'
 
 import Header from '../components/Header'
@@ -12,6 +13,14 @@ const Home: React.FC = () => {
       </Head>
       <Header />
 
+      <img
+        src="doctor-js.png"
+        alt="Doctor JS"
+        style={{
+          height: '50%',
+          width: 'auto'
+        }}
+      />
       <h1>abner.is()</h1>
       <Name>Abnêr Borgonha</Name>
       <p>This is my profile</p>
